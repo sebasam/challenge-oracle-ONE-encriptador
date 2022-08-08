5e11ef8dca8d3d620c4926a133b09f3330f22965
sebasam <sebasamayaxd@hotmail.com>
2022-08-07 21:37:22 -0500

    copy button

--- 1-challenge-encriptador/assets/js/index.js
+++ 1-challenge-encriptador/assets/js/index.js
@@ -1,9 +1,14 @@
 var textArea = document.querySelector('textarea')
 var btnEncript = document.querySelector('.btn-encript')
 var btnUnencript = document.querySelector('.btn-unencript')
+var btnCopy = document.getElementById('btn-copy')
 var text = document.getElementById('text-message')
 var textContainer = document.getElementById('text-content')
 var textMessage = document.getElementById('text-message')
+var hideElement = document.getElementById('hide')
+var showElement = document.querySelector('.show')
+var spanCrypt = document.getElementById('crypt-message')
+var messageContainer = document.getElementById('message')
 var message = ''
 var cryptMessage = ''
 //expresion regular para asegurar que el usuario ingrese unicamente espacios y letras minusculas
@@ -34,16 +39,30 @@ function encriptText(){
             }
         }
         setTimeout(function(){
-            textMessage.innerHTML = `<span>${ cryptMessage }</span>
-                                    <button>
-            `
+            showElement.classList.remove('show')
+            showElement.classList.add('hide-element')
+            hideElement.classList.remove('hide-element')    
+            hideElement.classList.add('show-element')        
             textArea.value = ''
+            spanCrypt.value = `${cryptMessage}`
         }, 500)
     }else{
         textContainer.lastElementChild.innerHTML = '<span style="color:red;">Debes escribir un texto para encriptar o desencriptar!</span>'
     }
 }
 
+function copyEncriptText(){
+    spanCrypt.select()
+    document.execCommand('copy')
+    hideElement.lastElementChild.innerHTML = `<span style="color: green; background-color: #b0b0c5; margin-top:5px;">Texto copiado!</span>`
+}
+
+
+
 btnEncript.addEventListener('click', function(){
     encriptText()
+})
+
+btnCopy.addEventListener('click', function(){
+    copyEncriptText()
 })
